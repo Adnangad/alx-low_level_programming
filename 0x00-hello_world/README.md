71c6dad3717b56b87f37eb92676fc22d2ce69b29
Adnangad <gardobuyaadnan@gmail.com>
2023-09-07 04:53:15 -0700

    task0

---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,1 @@
+creating a script that preprocesses a file and saves it to another file
---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,16 @@
+/**
+ * main.c
+ * Author:Adnan
+ * Description:This is shows the main contents of a c file
+ * Date:September 7th, 2023
+ */
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return always 0
+ */
+int main(void){
+	return 0;
+}
+
