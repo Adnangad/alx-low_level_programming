4e8e30d7ba862370f3edb625d9dda2da0a22802b
Adnangad <gardobuyaadnan@gmail.com>
2023-09-08 03:28:49 -0700

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,10 +5,10 @@
  */
 #include <stdio.h>
 /**
- * main Prints "with proper grammar, but the outcome is
- *              a piece of art,"followed by a new line
+ * main - Prints some given words
  *
- * Return : Always 0.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
