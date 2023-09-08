4fbf7f7469977af9816ea148e8366ca64d5c9461
Adnangad <gardobuyaadnan@gmail.com>
2023-09-08 11:28:38 -0700

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+/*
+ * File:5-print_numbers.c
+ * Author:Adnan
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints numbers from 0 to 10
+ *
+ * Return:Always 0.
+ */
+int main(void)
+{
+	int number = 0;
+
+	while (number < 10)
+	{
+		printf("%d\n", number);
+		number++;
+	}
+
+
+	return (0);
+}
