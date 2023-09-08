ac72850985ea9bf4ed65e11f8c2c982f97fe2110
Adnangad <gardobuyaadnan@gmail.com>
2023-09-08 03:47:02 -0700

    task6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+/*
+ * File:6-size.c
+ * Author:Adnan Gard
+ */
+#include <stdio.h>
+/**
+ * main -prints size of various parts
+ *
+ * Return:Always 0;
+ */
+int main(void)
+{
+	printf("Size of char:%zu byte(s)\n" ,sizeof(char));
+	printf("Size of an int:%zu byte(s)\n" ,sizeof(int));
+	printf("Size of a long int:%zu byte(s)\n",sizeof(long int));
+	printf("Size of a long long int:%zu byte(s)\n",sizeof(long long int));
+	printf("Size of a float:%zu byte(s)\n",sizeof(float));
+	return (0);
+}	
