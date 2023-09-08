4e6a15a0c57462c517372d7efcd174d4cc00048b
Adnangad <gardobuyaadnan@gmail.com>
2023-09-08 03:16:58 -0700

    task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+/*
+ * File:5-prints.c
+ * Author:Adnan Gard
+ */
+#include <stdio.h>
+/**
+ * main Prints given texts of words
+ *
+ * Return : Always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
