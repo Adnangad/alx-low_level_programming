91512a451055b59e610391715ddd84f60970971e
Adnangad <gardobuyaadnan@gmail.com>
2023-09-08 11:00:14 -0700

    task4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,25 +1,29 @@
 /*
- * File:4-print_alphabt.c
- * Author:Adnan
+ * File: 4-print_alphabt.c
+ * Author: Adnan
  */
+
 #include <stdio.h>
+
 /**
- * main - prints all letters except q and e
+ * main - Prints all letters except 'q' and 'e'
  *
- * Return:Always 0.
+ * Return: Always 0.
  */
 int main(void)
 {
-	char small = 'a';
-	while ('a' <= 'z' )
-	{
-		if (small !='e'&& small !='q')
-		{
-			putchar(small);
-			small++;
-		}
-	}
-	putchar('\n');
-	return (0);
-}
+    char small = 'a';
 
+    while (small <= 'z')
+    {
+        if (small != 'e' && small != 'q')
+        {
+            putchar(small);
+        }
+        small++;
+    }
+
+    putchar('\n');
+
+    return (0);
+}
