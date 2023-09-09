506b0dd06cfa470b1e5565fe7bc98fa5d99cb1fa
Adnangad <gardobuyaadnan@gmail.com>
2023-09-09 05:48:27 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,15 @@
-/*
- * File:4-puts.c
- * Author:Adnan
- * Description:program prints text
- * Date:September 7th 2023
- */
 #include <stdio.h>
+
 /**
- * main - Prints "Programming is like building a multilingual
- *                puzzle,followed by a new line
+ * main - Entry point of the program
  *
- * Return: Always 0.
+ * Description: This program prints "Programming is like building a multilingual puzzle"
+ *              followed by a new line.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
-{	
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
