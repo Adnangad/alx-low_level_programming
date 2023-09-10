b16242077706e6fd9e490674669a4456166206d1
Adnangad <gardobuyaadnan@gmail.com>
2023-09-10 04:49:53 -0700

    task7

--- 0x00-hello_world/0-main.c
+++ 0x00-hello_world/0-main.c
@@ -9,5 +9,10 @@
 int main(void)
 {
 	printf("Hello");
+	printf("Adnan Gard");
+	char name ='a';
+
+	putchar(name);
+
 	return (0);
 }
--- 0x00-hello_world/0-main.s
+++ 0x00-hello_world/0-main.s
@@ -3,6 +3,8 @@
 	.section	.rodata
 .LC0:
 	.string	"Hello"
+.LC1:
+	.string	"Adnan Gard"
 	.text
 	.globl	main
 	.type	main, @function
@@ -15,11 +17,19 @@ main:
 	.cfi_offset 6, -16
 	movq	%rsp, %rbp
 	.cfi_def_cfa_register 6
+	subq	$16, %rsp
 	leaq	.LC0(%rip), %rdi
 	movl	$0, %eax
 	call	printf@PLT
+	leaq	.LC1(%rip), %rdi
 	movl	$0, %eax
-	popq	%rbp
+	call	printf@PLT
+	movb	$97, -1(%rbp)
+	movsbl	-1(%rbp), %eax
+	movl	%eax, %edi
+	call	putchar@PLT
+	movl	$0, %eax
+	leave
 	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
