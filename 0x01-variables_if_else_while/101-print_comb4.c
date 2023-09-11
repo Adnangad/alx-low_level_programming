a76fdc6fa7f51cfbf1f8c0d3cd020a2fb9a53423
Adnangad <gardobuyaadnan@gmail.com>
2023-09-11 02:26:38 -0700

    task11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,33 +11,34 @@
  * Return:Always 0.
  */
 int main(void)
-{       
-        int num = 0;
-        
-        while (num < 8)
-	{
-		int num2 = num + 1;
+{
+    int num = 0;
 
-		while (num2 < 9)
-		{
-			int num3= num2 + 1;
+    while (num < 8)
+    {
+        int num2 = num + 1;
 
-			while (num3 <= 9)
-			{
-				putchar(num + '0');
-				putchar(num2 + '0');
-				putchar(num3 + '0');
-				if (num != 7 || num2 != 8 || num3 != 9)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-				num3++;
-			}
-			num2++;
-		}
-		num++;
-	}
-	putchar('\n');
-	return (0);
+        while (num2 < 9)
+        {
+            int num3 = num2 + 1;
+
+            while (num3 <= 9)
+            {
+                putchar(num + '0');
+                putchar(num2 + '0');
+                putchar(num3 + '0');
+                if (num != 7 || num2 != 8 || num3 != 9)
+                {
+                    putchar(',');
+                    putchar(' ');
+                }
+                num3++;
+            }
+            num2++;
+        }
+        num++;
+    }
+    putchar('\n');
+    return (0);
 }
+
