1d04c2fd5778c3d90701086dc4ad496fb3f1f7dd
Adnangad <gardobuyaadnan@gmail.com>
2023-09-12 08:36:24 -0700

    task3

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -1,19 +1,26 @@
+/*
+ * File:3-main.c
+ * Author:Adnan
+ */
+
+#include "main.h"
+#include <stdio.h>
+
 /**
- * _islower(int c) - prints lower
+ * main - checks for lowercase
  *
  * Return:Always 0.
  */
-#include "main.h"
-#include <ctype.h>
-
-int _islower(int c)
+int main(void)
 {
-	if (islower(c))
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+	int r;
+
+	r = _islower('H');
+	putchar(r + '0');
+	r = _islower('o');
+	putchar(r + '0');
+	r = _islower(108);
+	putchar(r + '0');
+	putchar('\n');
+	return (0);
 }
