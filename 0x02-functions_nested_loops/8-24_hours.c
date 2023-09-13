1af244459838a7f8c572b42f14562b0438c78679
Adnangad <gardobuyaadnan@gmail.com>
2023-09-13 05:26:43 -0700

    task8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+/*
+ * File:8-24_hours.c
+ * Author:Adnan
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * jack_bauer - prints minutes of the day
+ *
+ * Return:time
+ */
+void jack_bauer(void)
+{
+	int hours = 0;
+
+	while (hours <= 24)
+	{
+		int mins = 0;
+
+		while (mins <= 59)
+		{
+			printf("%02d:%02d\n", hours , mins);
+			mins++;
+		}
+		hours++;
+	}
+	printf("\n");
+}
---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,18 @@
+/*
+ * File:8-main.c
+ * Author:Adnan
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints time
+ *
+ * Return:Always 0.
+ */
+int main(void)
+{
+	jack_bauer();
+	return (0);
+}
