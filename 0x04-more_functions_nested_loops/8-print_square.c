8c45766d9d0a204b1d9fff6ea1d417bb52c7b242
Adnangad <gardobuyaadnan@gmail.com>
2023-09-15 07:45:16 -0700

    task8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include "_putchar.h"
+
+/**
+ * print_square - prints char depending on size
+ * @size:size to be checked
+ *
+ * Return:none.
+ */
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int d = 0;
+
+		while ( d < size)
+		{
+			int a = 0;
+
+			while (a < size)
+			{
+				_putchar('#');				
+				a++;
+			}
+			_putchar('\n');
+			d++;
+		}
+	}
+}
