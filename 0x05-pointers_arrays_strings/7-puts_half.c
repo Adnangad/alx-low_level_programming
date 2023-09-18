223938f8d1204a26b9322139699ceffbc688b38c
Adnangad <gardobuyaadnan@gmail.com>
2023-09-18 08:39:21 -0700

    task7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,13 +11,19 @@
  */
 void puts_half(char *str)
 {
-	int leng = strlen(str);
-	int i = leng / 2;
+	int index = 0, len = 0, n;
 
-	while (i < leng)
-                {
-                        putchar(str[i]);
-                        i += 1;
-                }
-        putchar('\n');
+	while (str[index++])
+		len++;
+
+	if ((len % 2) == 0)
+		n = len / 2;
+
+	else
+		n = (len + 1) / 2;
+
+	for (index = n; index < len; index++)
+		putchar(str[index]);
+
+	putchar('\n');
 }
