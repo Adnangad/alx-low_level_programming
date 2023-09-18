5ced43e2685db89f7a13ab729978ea1c3857b609
Adnangad <gardobuyaadnan@gmail.com>
2023-09-18 06:53:44 -0700

    task5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - reverses string
+ * @s:string to be rev
+ *
+ * Return:none.
+ */
+void rev_string(char *s)
+{
+	int leng = strlen(s);
+	int start = 0;
+	int end = leng - 1;
+
+	while (start < end)
+	{
+		char and = s[start];
+		s[start] = s[end];
+		s[end] = and;
+		start++;
+		end--;
+	}
+}
