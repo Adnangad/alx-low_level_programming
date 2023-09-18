c34c94832ac77079e72a433b51968e558c740984
Adnangad <gardobuyaadnan@gmail.com>
2023-09-18 03:50:59 -0700

    task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - prints length of string
+ * @s:string to be det
+ *
+ * Return:Length of string
+ */
+int _strlen(char *s)
+{
+	int len = strlen(s);
+
+	return (len);
+}
