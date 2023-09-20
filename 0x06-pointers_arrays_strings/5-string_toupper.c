4aeb63839216b3c7557ce4a24dff361ff5a1a99b
Adnangad <gardobuyaadnan@gmail.com>
2023-09-20 06:31:47 -0700

    task5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * string_toupper - converts lowercase to uppercase
+ * @s:string to be checked
+ *
+ * Return:caps
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while(s[i] != '\0')
+	{
+		s[i] = (char)toupper((int)s[i]);
+		i++;
+	}
+	return (s);
+}
