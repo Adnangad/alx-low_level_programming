c3fba401410d1639e8a74bc407e4287ca0d5205d
Adnangad <gardobuyaadnan@gmail.com>
2023-09-20 07:17:04 -0700

    task7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include "_putchar.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * leet - encodes diff letters
+ * @s:string to be encoded
+ *
+ * Return:s.
+ */
+char *leet(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == 'a' || s[i] == 'A')
+		{
+			s[i] = '4';
+		}
+		if (s[i] == 'e' || s[i] == 'E')
+                {       
+                        s[i] = '3';
+		}
+		if (s[i] == 'o' || s[i] == 'O')
+                {
+                        s[i] = '0';
+                }
+		if (s[i] == 't' || s[i] == 'T')
+                {
+                        s[i] = '7';
+		}
+		if (s[i] == 'l' || s[i] == 'L')
+                {
+                        s[i] = '1';
+                }
+		i++;
+	}
+	return (s);
+}
