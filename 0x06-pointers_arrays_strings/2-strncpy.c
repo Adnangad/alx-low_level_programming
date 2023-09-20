b862065ff057e8898940edff831c06d40e52232e
Adnangad <gardobuyaadnan@gmail.com>
2023-09-20 01:42:40 -0700

    task2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncpy:copies strings
+ * @dest:string to be copied to
+ * @src:string to be copied from
+ * @n:max number of bytes from src
+ *
+ * Return:capy.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *capy;
+
+	capy = strncpy(dest, src, n);
+
+	return (capy);
+}
