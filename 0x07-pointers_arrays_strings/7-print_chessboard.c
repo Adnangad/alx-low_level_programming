71efad266a4bb0da899d165b45420f43c71bebec
Adnangad <gardobuyaadnan@gmail.com>
2023-09-25 02:53:40 -0700

    task6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_chessboard - prints chessboard
+ * *a:array called a
+ *
+ * Return:none.
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			printf("%c", a[i][j]);
+		}
+		printf("\n");
+	}
+	if (i != 8)
+	{
+		printf("\n");
+	}
+}
