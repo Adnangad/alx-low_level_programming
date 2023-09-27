bad737c2dc2fdf8577d87a1e436185a63ddf0c0a
Adnangad <gardobuyaadnan@gmail.com>
2023-09-27 11:06:26 -0700

    task5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,28 +1,27 @@
 #include "main.h"
 #include <math.h>
 
-int _sqrt_recursion(int n);
+
 /**
  *  actual - finds the squareroot
  *  @n:number to be det
+ *  @square:no to check
  *
  *  Return:-1 if no squareroot
  */
-int actual(int n)
+int actual(int n, int square)
 {
-	double square = sqrt(n);
-
-	if (square < 0)
+	if (n * n == square)
 	{
-		return -1;
+		return (n);
 	}
-	else if (square != floor(square))
+	else if (square < n * n)
 	{
-		return -1;
+		return (-1);
 	}
 	else
 	{
-		return square;
+		return actual(n + 1, square);
 	}	
 }
 
@@ -34,9 +33,5 @@ int actual(int n)
  */
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-	{
-		return -1;
-	}
-	return actual(n);
+	return actual(1, n);
 }
