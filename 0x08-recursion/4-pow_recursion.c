00aff0d0e476cbd6852a2c8ada729268f4916cef
Adnangad <gardobuyaadnan@gmail.com>
2023-09-27 07:16:38 -0700

    task4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <math.h>
+
+/**
+ * _pow_recursion - raises numbers to power
+ * @x:base number
+ * @y:the value to be raised
+ *
+ * Return:- 1 if y < 0.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if ( y == 0)
+	{
+		return (1);
+	}
+	{
+		return x * _pow_recursion(x, y - 1);
+	}
+}
