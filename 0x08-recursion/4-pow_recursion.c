4e285e1b00c9e603bec4380b32df9624ecadc08a
Adnangad <gardobuyaadnan@gmail.com>
2023-09-27 10:49:16 -0700

    task4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -6,17 +6,17 @@
  * @x:base number
  * @y:the value to be raised
  *
- * Return:- 1 if y < 0.
+ * Return:x to power y.
  */
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 	{
-		return -1;
+		return (-1);
 	}
-	else if ( y <= 0)
+	else if ( y == 0)
 	{
-		return 1;
+		return (1);
 	}
-	return x * _pow_recursion(x, y - 1);
+	return (x * _pow_recursion(x, y - 1));
 }
