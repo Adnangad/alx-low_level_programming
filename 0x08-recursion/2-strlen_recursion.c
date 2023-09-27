26e69ce8ad660e7f4243e2952c0af7b0c03babb0
Adnangad <gardobuyaadnan@gmail.com>
2023-09-27 06:27:17 -0700

    task2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include "_putchar.h"
+#include <string.h>
+
+/**
+ *  _strlen_recursion - prints strlength
+ *  @s:string to be counted
+ *
+ *  Return:length
+ */
+int _strlen_recursion(char *s)
+{
+	int length = strlen(s);
+
+	return (length);
+}
