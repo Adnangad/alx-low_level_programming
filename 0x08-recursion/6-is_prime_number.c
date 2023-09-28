1a5eafab4bee02d1e1699cd4d3df7bfbf473b8aa
Adnangad <gardobuyaadnan@gmail.com>
2023-09-28 02:04:01 -0700

    task6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,28 @@
 #include "main.h"
 
-
+/**
+ * helper - stack func
+ * @n:number to be checked
+ * @i:divisor
+ *
+ * Return: n divided by i.
+ */
+int helper(int n, int i)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else if (i * i > n)
+	{
+		return (1);
+	}
+	else if (n % i == 0)
+	{
+		return (0);
+	}
+	return (helper(n, i + 2));
+}
 /**
  * is_prime_number - checks for prime no
  * @n:int to be checked
@@ -9,19 +31,13 @@
  */
 int is_prime_number(int n)
 {
-	int i = 3;
-
-	if (n <= 1)
+	if (n == 2)
+	{
+		return (1);
+	}
+	else if (n <= 1 || n % 2 == 0)
 	{
 		return (0);
 	}
-	while (i * i <= n)
-	{
-		if (n % i == 0)
-		{
-			return (0);
-		}
-		i++;
-	}
-	return (1);
+	return (helper(n, 3));
 }
