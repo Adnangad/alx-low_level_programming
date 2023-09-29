23cdf65501399f573468a2c9307cec0557d19bb2
Adnangad <gardobuyaadnan@gmail.com>
2023-09-29 04:28:47 -0700

    task3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the no of arg
+ * @argc:no of arg
+ * @argv:contents of arg
+ *
+ * Return:Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i = 1;
+	int mul = 1;
+
+	if (argc > 1)
+	{
+		while (i < argc)
+		{
+			mul *= atoi(argv[i]);
+			i++;
+		}
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
