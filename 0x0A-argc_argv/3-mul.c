97f66388ea8478a2c9f5d5213ae6ae703b62be46
Adnangad <gardobuyaadnan@gmail.com>
2023-09-29 04:33:35 -0700

    task3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,6 +13,11 @@ int main(int argc, char *argv[])
 	int i = 1;
 	int mul = 1;
 
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
 	if (argc > 1)
 	{
 		while (i < argc)
@@ -21,11 +26,6 @@ int main(int argc, char *argv[])
 			i++;
 		}
 		printf("%d\n", mul);
-	}
-	else
-	{
-		printf("Error\n");
-		return (1);
 	}
 	return (0);
 }
