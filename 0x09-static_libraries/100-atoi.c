894c8a108fe3a2d5b6570ba8cc69a4c72b2e0c29
Adnangad <gardobuyaadnan@gmail.com>
2023-09-29 08:10:48 -0700

    task1

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,6 +1,12 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * _atoi - converts string to int
+ * @s:string to be conv
+ *
+ * Return:ats
+ */
 int _atoi(char *s)
 {
 	int ats = atoi(s);
--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,6 +1,12 @@
 #include "main.h"
 #include  <stdlib.h>
 
+/**
+ * _abs - finds absolute value
+ * @n:int to be checked
+ *
+ * Return:ab.
+ */
 int _abs(int n)
 {
 	int ab = abs(n);
