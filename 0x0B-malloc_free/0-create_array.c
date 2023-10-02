24945b097118ae9cbca3122b3ac900ecde56d203
Adnangad <gardobuyaadnan@gmail.com>
2023-10-02 00:22:11 -0700

    task0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * create_array - creates array of chars
+ * @size:strlength of c
+ * @c:contains string
+ *
+ * Return:pointer or null.
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *p;
+
+	p = (char*)malloc(size * sizeof(char));
+
+	for (i = 0; i < size; i++)
+	{
+		p[i] = c;
+	}
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		return (p);
+	}
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+0-create a func that creates an array of chars and initializes it with specific elements
