72868d5445e0fae46848833614642bb53601c2b9
Adnangad <gardobuyaadnan@gmail.com>
2023-10-02 02:34:21 -0700

    task2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * str_concat - returns pointer to mem
+ * @s1:str to be concat to
+ * @s2:sttr to be concat from
+ *
+ * Return:concat.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int size = strlen(s1) + strlen(s2) + 1;
+	char *str = (char *)malloc(size * sizeof(char));
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(str, s1);
+	strcat(str, s2);
+	return (str);
+}
