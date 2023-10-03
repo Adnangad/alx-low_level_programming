d9fbd42939c56ca26327de7425bf4e3e374ab709
Adnangad <gardobuyaadnan@gmail.com>
2023-10-03 03:41:43 -0700

    grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -30,11 +30,6 @@ int **alloc_grid(int width, int height)
 
 		if (point[i] == NULL)
 		{
-			for (j = 0; j < i; j++)
-			{
-				free(point[j]);
-			}
-			free(point);
 			return (NULL);
 		}
 		for (j = 0; j < width; j++)
