2a5db95ca330ab89f2727f6b39cd869e49f2f4b3
Adnangad <gardobuyaadnan@gmail.com>
2023-10-05 02:34:06 -0700

    task3-array

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,14 +10,13 @@
  */
 int *array_range(int min, int max)
 {
-	int *point, i;
-
-	int size = max - min + 1;
+	int i, *point, size;
 
 	if (min > max)
 	{
 		return (NULL);
 	}
+	size = max - min + 1;
 	point = (int*)malloc(size * sizeof(int));
 
 	if (point == NULL)
