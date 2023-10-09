fb349d136b582db90bb5a908cb7f709368b6da70
Adnangad <gardobuyaadnan@gmail.com>
2023-10-09 05:49:52 -0700

    1-func

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,7 +12,7 @@
  * Description:initializes struct dog with provided data
  *
  */
-void init_dog(struct dog *d, char *name, float age, char *owner)
+void init_dog(struct dog *d, char* name, float age, char* owner)
 {
 	if (d != NULL)
 	{
