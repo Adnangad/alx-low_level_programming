fadc8d522b70924ef8266cb3b887e0dba488f608
Adnangad <gardobuyaadnan@gmail.com>
2023-10-09 05:35:55 -0700

    1-func

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -3,12 +3,14 @@
 
 /**
  * init_dog - initializes a var
+ *
  * @d:struct name
  * @name:dog name
  * @age:dogs age
  * @owner:owners age
  *
- * Return:none
+ * Description:initializes struct dog with provided data
+ *
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
