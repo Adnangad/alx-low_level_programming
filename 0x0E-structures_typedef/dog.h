ef3f313bcee075b622e71ae0245ba027016152e0
Adnangad <gardobuyaadnan@gmail.com>
2023-10-09 06:46:09 -0700

    3-typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,19 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- * struct dog - contains details of a dog
+ * dog_t - contains details of a dog
  * @name:dogs name
  * @age:dogs age
  * @owner:dogs owner
  *
  * Description: i assign info based on given data types
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
