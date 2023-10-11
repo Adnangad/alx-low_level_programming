8650fcc66d5a874b49315931172185bf6a2b0c3a
Adnangad <gardobuyaadnan@gmail.com>
2023-10-11 11:28:02 -0700

    3-calc

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -20,19 +20,17 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-	sign = get_op_func(argv[2]);
-
+	if (argc == 4)
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[3]);
+		si = argv[2];
+		sign = get_op_func(si);
+	}
 	if (!sign)
 	{
 		printf("Error\n");
 		exit(99);
-	}
-	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
-	{
-		printf("Error\n");
-		exit(100);
 	}
 	answer = sign(num1, num2);
 
