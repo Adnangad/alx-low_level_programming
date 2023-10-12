30367bc76ad948eaf95d6b2e20a8608fc16e3580
Adnangad <gardobuyaadnan@gmail.com>
2023-10-12 07:52:25 -0700

    3-calc

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,31 +1,29 @@
+#include <stdio.h>
 #include "3-calc.h"
-#include <stdlib.h>
 #include <string.h>
-
 /**
- * get_op_func - selects the correct func to perform the operation
- * @s:operator passed to the argument
- *
- * Return:Null.
+ * get_op_func - selects the correct function to perform the operation
+ * @s: the operator passed to the function
+ * Return:null
  */
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
 	int i;
 
-	for (i = 0; ops[i].op; i++)
+	i = 0;
+	while (i < 5)
 	{
 		if (strcmp(s, ops[i].op) == 0)
-		{
 			return (ops[i].f);
-		}
+		i++;
 	}
 	return (NULL);
 }
