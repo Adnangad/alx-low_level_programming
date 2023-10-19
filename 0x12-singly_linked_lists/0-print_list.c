a77776342f707fe9cdf969a118b21b753a908e18
Adnangad <gardobuyaadnan@gmail.com>
2023-10-19 03:48:15 -0700

    tsk-0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,11 +12,14 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		if (h-> str == NULL)
+		if (h->str == NULL)
 		{
 			printf("[0] (nil)\n");
 		}
-		printf("[%u] %s\n", h->len, h->str);
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
 		h = h->next;
 		i++;
 	}
