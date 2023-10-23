57fd5c0fed07380a6682ce9c3f03758adeec0e32
Adnangad <gardobuyaadnan@gmail.com>
2023-10-23 03:03:26 -0700

    task-0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,19 +9,12 @@
 size_t print_listint(const listint_t *h)
 {
 	int count = 0;
-	listint_t *help = NULL;
 
-	if (h == NULL)
+	while (h != NULL)
 	{
-		return (NULL);
-	}
-	help = h;
-
-	while (help != NULL)
-	{
-		count++;
 		printf("%d\n", h->n);
-		help = help->next;
+		h = h->next;
+		count++;
 	}
        return (count);
 }
