c8841c7d00d1fdcc41b0f73e8f3cb16540adeed6
Adnangad <gardobuyaadnan@gmail.com>
2023-10-24 04:17:27 -0700

    task-9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -36,7 +36,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		help = help->next;
 		idx--;
 	}
-	if (help == NULL)
+	if (idx != 1)
 	{
 		free(new);
 		return (NULL);
