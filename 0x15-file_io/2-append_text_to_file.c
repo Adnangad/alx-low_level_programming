9e8bbb0fd497bbe23c701a41b9e7f174821f46f6
Adnangad <gardobuyaadnan@gmail.com>
2023-10-30 08:18:10 -0700

    task-2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename:pointer to file name
+ * @text_content:text to be appended
+ *
+ * Return:1 on success
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	FILE *app;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	app = fopen(filename, "a");
+
+	if (app == NULL)
+	{
+		return (-1);
+	}
+	if (text_content == NULL)
+	{
+		fclose(app);
+	}
+	fprintf(app, "%s", text_content);
+	fclose(app);
+	return (1);
+}
