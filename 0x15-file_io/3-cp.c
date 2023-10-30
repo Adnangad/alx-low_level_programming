456f18a3b37b9d865ea213cdc009a9cffed27bde
Adnangad <gardobuyaadnan@gmail.com>
2023-10-30 11:39:12 -0700

    task-3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -21,19 +21,27 @@ int main(int argc, char *argv[])
 	char buf[BUF_SIZE];
 
 	if (argc != 3)
+	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	}
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	}
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, own);
 	if (fd_to == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	}
 	rd_stat = 1;
 	while (rd_stat)
 	{
 		rd_stat = read(fd_from, buf, BUF_SIZE);
 		if (rd_stat == -1)
+		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+		}
 		if (rd_stat > 0)
 		{
 			wr_stat = write(fd_to, buf, rd_stat);
