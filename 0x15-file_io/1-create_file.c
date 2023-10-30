8a3a6e43a92b5f4e7ef1b66490e964fb35ab5d3b
Adnangad <gardobuyaadnan@gmail.com>
2023-10-30 08:34:40 -0700

    task-1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename:pointer to name offile
+ * @text_content:string to be written
+ *
+ * Return:1 on success
+ */
+int create_file(const char *filename, char *text_content)
+{
+	FILE *wrt;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	if (text_content == NULL)
+	{
+		return (1);
+	}
+	wrt = fopen(filename, "w");
+
+	if (wrt == NULL)
+	{
+		return (-1);
+	}
+	fprintf(wrt, "%s", text_content);
+	fclose(wrt);
+	return (1);
+}
