209187a4fa0518cf62b12bd4b333fa7ad54b664c
Adnangad <gardobuyaadnan@gmail.com>
2023-12-07 04:18:47 -0800

    task-6

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - sums the number data in each node
+ * @head:pointer to head node
+ *
+ * Return:sum
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int i = 0;
+	dlistint_t *c = head;
+
+	while (c->next != NULL)
+	{
+		c = c->next;
+		i += c->n;
+	}
+	return (i);
+}
