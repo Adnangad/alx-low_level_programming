5c188c9340ff2eec67979c76573234f6bba31b96
Adnangad <gardobuyaadnan@gmail.com>
2023-12-11 01:49:04 -0800

    task-100, python + c dynamic lib

---
+++ 0x18-dynamic_libraries/calc.c
@@ -0,0 +1,57 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers
+ * @a:first no
+ * @b:second num
+ *
+ * Return:sum of the two
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - subtracts two numbers
+ * @a:first no
+ * @b:second num
+ *
+ * Return:diference of the two
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - multiplies two numbers
+ * @a:first no
+ * @b:second num
+ *
+ * Return:result of the two
+ */
+int mul(int a, int b)
+{
+        return (a * b);
+}
+/**
+ * div - divides two numbers
+ * @a:first no
+ * @b:second num
+ *
+ * Return:result of the two
+ */
+int div(int a, int b)
+{
+        return (a / b);
+}
+/**
+ * mod - divides two numbers and gets rem
+ * @a:first no
+ * @b:second num
+ *
+ * Return:remainder
+ */
+int mod(int a, int b)
+{
+        return (a % b);
+}
