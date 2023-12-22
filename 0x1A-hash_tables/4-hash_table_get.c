156fb75fdf8dcda97a8954ead0652d7bbd868d03
Adnangad <gardobuyaadnan@gmail.com>
2023-12-22 06:13:05 -0800

    task-4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,14 +9,21 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int idx = key_index((const unsigned char*)key, ht->size);
+	unsigned long int idx;
 
-	if (ht->array[idx] != NULL)
+	if (ht == NULL || key == NULL || strlen(key) == 0)
+	{
+		return (NULL);
+	}
+	idx = key_index((const unsigned char*)key, ht->size);
+
+	while (ht->array[idx] != NULL)
 	{
 		if (strcmp(ht->array[idx]->key, key) == 0)
 		{
 			return (ht->array[idx]->value);
 		}
+		idx = (idx + 1) % ht->size;
 	}
 	return (NULL);
 }
